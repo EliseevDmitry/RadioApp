9ace8f79f6e960d2ed06d88bdb1624f03777c2cc
DmKeller <dmit.keller@gmail.com>
2024-07-28 23:15:38 +0300

    add colors

--- RadioApp/Resources/DesignSystem/DS.swift
+++ RadioApp/Resources/DesignSystem/DS.swift
@@ -5,11 +5,21 @@
 //  Created by Келлер Дмитрий on 28.07.2024.
 //
 
-import Foundation
+import SwiftUI
 
+// MARK: - DS (Design System)
 enum DS {
     enum Colors {
-        
+        /// темноСиний.
+        static let darkBlue = Color(hex: "#01012A")
+        /// бирюзовый
+        static let turquoise = Color(hex: "#005679")
+        /// голубойНеоновый.
+        static let blueNeon = Color(hex: "#05D8E8")
+        /// бледно голубой.
+        static let blueLight = Color(hex: "#D1F9FF")
+        /// розовыйНеоновый.
+        static let pinkNeon = Color(hex: "#FF296D")
     }
     
     enum Fonts {
