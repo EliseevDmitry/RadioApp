c98abdeb706770372be446fd49b914c6b525e182
Evgeniy Kukarskiy <ekukarskiy@gmail.com>
2024-07-29 22:31:36 +0500

    ViewModel & NetworkServices

--- RadioApp/Models/ViewModel.swift
+++ RadioApp/Models/ViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+@MainActor
+final class ViewModel: ObservableObject {
+
+    @Published var stations = [Station]()
+    // добавляем сюда другие @Published свойства
+
+    let network = NetworkService()
+
+    func fetchTopStations() async throws {
+        var fetchedStations: [Station]
+        fetchedStations = try await network.getTopStations(numberLimit: 15)
+        stations = fetchedStations
+    }
+}
