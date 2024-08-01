f78d474356efe6dc2ce2792e8e032b30bffee481
denisgindulin <denisgindulin@me.com>
2024-08-01 17:34:20 +0300

    fix firebase-uikit bug

--- RadioApp/App/RadioAppApp.swift
+++ RadioApp/App/RadioAppApp.swift
@@ -10,22 +10,29 @@ import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+//        return true
+//    }
+//}
 
 @main
 struct RadioAppApp: App {
     //@StateObject var appManager = ViewModel()
     @StateObject var appManager = ViewModel()
     // register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
     
     var body: some Scene {
+    
+        
         WindowGroup {
+//      
+//                    FirebaseApp.configure()
+
 //            WelcomeView()
 //                .environmentObject(appManager)
            // TestView(model: appManager)
