234bf37335ed36fb0c0a4d6561b448b15e3575a5
DmKeller <dmit.keller@gmail.com>
2024-08-09 17:29:57 +0300

    fix nav

--- RadioApp/Flows/Autorization/SignInView.swift
+++ RadioApp/Flows/Autorization/SignInView.swift
@@ -8,83 +8,129 @@
 import SwiftUI
 
 struct SignInView: View {
+    // MARK: - Properties
     @EnvironmentObject var appManager: ViewModel
     
     @State private var isAuthenticated = false
     @State private var showAlert = false
-    @State private var alert: AnyAppAlert?
-    @State private var errorAlert: AnyAppAlert? = nil
+    @State private var alertMessage = ""
     
+    // MARK: - Body
     var body: some View {
-        ZStack {
-            AnimatedBackgroundView()
-            AuthBackgroundView()
+        NavigationView {
             
-            VStack(alignment: .leading) {
-                Spacer()
+            ZStack {
+                // Backgrounds
+                AnimatedBackgroundView()
+                AuthBackgroundView()
                 
-                Image("Group 3").resizable()
-                    .frame(width: UIScreen.width * 1/4, height: UIScreen.width * 1/4)
-                Text(Resources.Text.SignIn.title)
-                    .font(.custom(.sfBold, size: UIScreen.height * 1/16))
-                    .padding(.bottom, UIScreen.height * 1/32)
-                
-                Text(Resources.Text.SignIn.toStartPlay)
-                    .font(.custom(.sfRegular, size: UIScreen.height * 1/48))
-                    .frame(maxWidth: UIScreen.width * 1/3)
-                
-                TextField(Resources.Text.SignIn.email, text: $appManager.email)
-                    .font(.title)
-                
-                SecureField(Resources.Text.SignIn.password, text: $appManager.password)
-                    .font(.title)
-                
-                Text(Resources.Text.SignIn.orConnectWith)
-                
-                Button(action: {}) {
-                    Text("G+")
-                        .foregroundStyle(.white)
-                }
-                
-                CustomButton(action: {
-                    Task {
-                        await signIn()
-                    }
-                },
-                             title: Resources.Text.SignIn.title,
-                             buttonType: .onboarding
-                )
-                // TODO: добавить в типы кнопок третий тип - для этой группы экранов и при выборе типа заменить тернарный оператор на switch (обсудить с Димой Келлером)
-                
-                Button(action: {
+                VStack(alignment: .leading) {
+                    Spacer()
                     
-                }) {
-                    Text(Resources.Text.SignIn.orSignUp)
-                        .foregroundStyle(.white)
-                        .showCustomAlert(alert: $alert)
-                        .showCustomAlert(alert: $alert)
-                        .background(
-                            NavigationLink(destination: ContentView(), isActive: $isAuthenticated) { EmptyView() }
-                        )
+                    appLogo
+                    
+                    titleText
+                    
+                    subtitleText
+                    
+                    inputFields
+                    
+                    socialMediaButton
+                    
+                    signInButton
+                    
+                    signUpButton
+                    
+                    Spacer()
                 }
-                
-                Spacer()
+                .padding()
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Error"),
+                        message: Text(alertMessage),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
             }
-            .padding()
+        }
+    }
+    // MARK: - Subviews
+    private var appLogo: some View {
+        Image("Group 3")
+            .resizable()
+            .frame(width: UIScreen.width * 1/4, height: UIScreen.width * 1/4)
+    }
+    
+    private var titleText: some View {
+        Text(Resources.Text.SignIn.title)
+            .font(.custom(.sfBold, size: UIScreen.height * 1/16))
+            .padding(.bottom, UIScreen.height * 1/32)
+    }
+    
+    private var subtitleText: some View {
+        Text(Resources.Text.SignIn.toStartPlay)
+            .font(.custom(.sfRegular, size: UIScreen.height * 1/48))
+            .frame(maxWidth: UIScreen.width * 1/3)
+    }
+    
+    private var inputFields: some View {
+        VStack {
+            TextField(Resources.Text.SignIn.email, text: $appManager.email)
+                .font(.title)
+            
+            SecureField(Resources.Text.SignIn.password, text: $appManager.password)
+                .font(.title)
         }
     }
     
+    private var socialMediaButton: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(Resources.Text.SignIn.orConnectWith)
+            
+            Button(action: {}) {
+                Text("G+")
+                    .foregroundStyle(.white)
+            }
+        }
+    }
+    
+    private var signInButton: some View {
+        CustomButton(action: {
+            Task {
+                await signIn()
+            }
+        },
+                     title: Resources.Text.SignIn.title,
+                     buttonType: .onboarding
+        )
+        .background(
+            NavigationLink(destination: ContentView(), isActive: $isAuthenticated) {
+                EmptyView()
+            }
+        )
+    }
+    
+    private var signUpButton: some View {
+        Button(action: {}) {
+            Text(Resources.Text.SignIn.orSignUp)
+                .foregroundStyle(.white)
+        }
+    }
+    
+    // MARK: - Functions
     private func signIn() async {
         await appManager.signIn()
-        if appManager.error != nil {
-            alert = AnyAppAlert(error: appManager.error!)
+        if let error = appManager.error {
+            alertMessage = error.localizedDescription
             showAlert = true
+            appManager.clearError()
         } else {
             isAuthenticated = true
         }
     }
 }
 
+// MARK: - Previews
 struct SignInView_Previews: PreviewProvider {
     static let previewAppManager = ViewModel()
     
@@ -93,4 +139,3 @@ struct SignInView_Previews: PreviewProvider {
             .environmentObject(previewAppManager)
     }
 }
-
