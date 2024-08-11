7392c17935efee32ee6ce8c6e476f99e51f966f9
Dmitry <dimasel@gmail.com>
2024-08-11 13:15:42 +0700

    Test 2

--- RadioApp/Screens/AllStationsView.swift
+++ RadioApp/Screens/AllStationsView.swift
@@ -8,61 +8,56 @@
 import SwiftUI
 
 struct AllStationsView: View {
-
     @EnvironmentObject var appManager: ViewModel
     @State private var isSearching: Bool = false
-
     var body: some View {
-        NavigationView {
-            VStack {
-                // text
+        VStack{
+            HStack {
+                Text("All stations")
+                    .font(.custom(DS.Fonts.sfRegular, size: 30))
+                    .foregroundStyle(.white)
+                Spacer()
+            }
+            .padding(.leading, 60)
+            .padding(.top, 100)
+            // search view
+            SearchBarView(isSearching: $isSearching)
+                .frame(height: 56)
+            Spacer()
+            HStack{
+                VolumeView(rotation: false)
+                    .frame(width: 33 ,height: 250)
+                    .padding(.leading, 15)
                 VStack {
-                    HStack {
-                        Text("All stations")
-                            .font(.custom(DS.Fonts.sfRegular, size: 30))
-                            .foregroundStyle(.white)
-                        Spacer()
-                    }
-                    .padding(.leading, 60)
-                    .padding(.top, 10)
-                    
-                    // search view
-                    SearchBarView(isSearching: $isSearching)
-                        .frame(height: 56)
-                    
-                    Spacer()
-                    HStack {
-                        // sound control
-                        VStack {
-                            VolumeView(rotation: false)
-                                .frame(width: 33 ,height: 250)
-                                .padding(.leading, 15)
-                        }
-                        
-                        // stations
-                        ScrollView(.vertical, showsIndicators: false) {
-                            LazyVStack(pinnedViews: .sectionHeaders) {
-                                ForEach(appManager.stations, id: \.stationuuid) { station in
-                                    NavigationLink {
-                                        StationDetailsView(station: station)
-                                        
-                                    } label: {
-                                        StationView(selectedStationID: $appManager.selectedStation, station: station)
-                                    }
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVStack(pinnedViews: .sectionHeaders) {
+                            ForEach(appManager.stations, id: \.stationuuid) { station in
+                                NavigationLink {
+                                    StationDetailsView(station: station)
                                     
+                                } label: {
+                                    StationView(selectedStationID: $appManager.selectedStation, station: station)
                                 }
+                                
                             }
-                            .background(DS.Colors.darkBlue)
-                            .navigationViewStyle(.stack)
                         }
-                        Spacer()
+                        .background(DS.Colors.darkBlue)
+                        .navigationViewStyle(.stack)
                     }
-                    Spacer()
                 }
-                .background(DS.Colors.darkBlue)
+                Spacer()
+            }
+            Spacer()
+        }
+        .background(DS.Colors.darkBlue)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                ToolbarName()
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                ToolbarProfile()
             }
         }
-        .padding(.top, 100)
         .task {
             do {
                 try await appManager.fetchAllStations()
@@ -77,7 +72,6 @@ struct AllStationsView: View {
         }
     }
 }
-
 
 //MARK: - PREVIEW
 struct AllStationsView_Previews: PreviewProvider {
--- RadioApp/Screens/PopularView.swift
+++ RadioApp/Screens/PopularView.swift
@@ -10,48 +10,49 @@ import SwiftUI
 struct PopularView: View {
     //MARK: - PROPERTIES
     @EnvironmentObject var appManager: ViewModel
-    @State private var isActive = false
     let columns = [
         GridItem(.flexible(minimum: 139, maximum: 139)),
         GridItem(.flexible(minimum: 139, maximum: 139))
     ]
     //MARK: - BODY
     var body: some View {
-        NavigationView {
-            VStack {
-                VStack {
-                    HStack {
-                        Text("Popular")
-                            .font(.custom(DS.Fonts.sfRegular, size: 30))
-                            .foregroundStyle(.white)
-                        Spacer()
-                    }
-                    .padding(.leading, 60)
-                    .padding(.top, 10)
-                    HStack{
-                        VolumeView(rotation: false)
-                            .frame(width: 33 ,height: 250)
-                            .padding(.leading, 15)
-                        
+            VStack{
+                HStack {
+                    Text("Popular")
+                        .font(.custom(DS.Fonts.sfRegular, size: 30))
+                        .foregroundStyle(.white)
+                    Spacer()
+                }
+                .padding(.leading, 60)
+                .padding(.top, 100)
+                .background(DS.Colors.darkBlue)
+                HStack{
+                    VolumeView(rotation: false)
+                        .frame(width: 33 ,height: 250)
+                        .padding(.leading, 15)
+                    VStack {
                         ScrollView(.vertical, showsIndicators: false){
                             LazyVGrid(columns: columns) {
                                 ForEach(appManager.stations, id: \.stationuuid) {item in
-                                  
-                                        StationPopularView(selectedStationID: $appManager.selectedStation, station: item)
-                                            .frame(width: 139, height: 139)
+                                    StationPopularView(selectedStationID: $appManager.selectedStation, station: item)
+                                        .frame(width: 139, height: 139)
                                 }
                             }
-                            .background(DS.Colors.darkBlue)
                         }
-                        Spacer()
                     }
                     Spacer()
                 }
-                .background(DS.Colors.darkBlue)
+                Spacer()
+            }
+        .background(DS.Colors.darkBlue)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                    ToolbarName()
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                ToolbarProfile()
             }
         }
-        .padding(.top, 100)
-        .navigationViewStyle(.stack)
         .task {
             do {
                 try await appManager.fetchTopStations()
